21e16cfe61c36175396d1db8ca5e980ffe174825
Eduard Dik <oddshellnick.programming@gmail.com>
2025-02-02 17:03:41 +0700

    1.3.13 release commit

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def get_description() -> str:
 
 setup(
 		name="PyWebRequests",
-		version="1.3.12",
+		version="1.3.13",
 		author="oddshellnick",
 		author_email="oddshellnick.programming@gmail.com",
 		description=get_description(),
